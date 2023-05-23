7bd092101daf079e56c884dff5fab76de324c42e
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2023-05-23 15:27:23 +0100

    Add moveto test

--- tests/I24/serial/extruder/test_extruder_collect.py
+++ tests/I24/serial/extruder/test_extruder_collect.py
@@ -2,6 +2,7 @@ from unittest.mock import patch
 
 from mx_bluesky.I24.serial.extruder.i24ssx_Extruder_Collect_py3v2 import (
     initialise_extruderi24,
+    moveto,
     scrape_parameter_file,
 )
 
@@ -15,3 +16,11 @@ def test_scrape_parameter_file():
 def test_initialise_extruder(fake_caput):
     initialise_extruderi24()
     assert fake_caput.call_count == 11
+
+
+@patch("mx_bluesky.I24.serial.extruder.i24ssx_Extruder_Collect_py3v2.caput")
+@patch("mx_bluesky.I24.serial.extruder.i24ssx_Extruder_Collect_py3v2.caget")
+def test_moveto(fake_caput, fake_caget):
+    moveto("enterhutch")
+    assert fake_caget.call_count == 1
+    assert fake_caput.call_count == 1
