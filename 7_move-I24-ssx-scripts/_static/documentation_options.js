82d376767214ef7ee23ff3d53907d430590e578a
noemifrisina <noemifrisina@users.noreply.github.com>
2023-05-23 14:19:19 +0000

    deploy: 868f097130a512313172926dbcbdbd544bbfbb0b

--- 7_move-I24-ssx-scripts/_static/documentation_options.js
+++ 7_move-I24-ssx-scripts/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev54+g49e718d',
+    VERSION: '0.1.dev55+g868f097',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
