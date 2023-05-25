1e84e4824dbcdfa0aa9760dbeb16b43324f1aaeb
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2023-05-25 13:51:34 +0100

    Setup_beamline test fix

--- tests/I24/serial/setup_beamline/test_setup_beamline.py
+++ tests/I24/serial/setup_beamline/test_setup_beamline.py
@@ -17,12 +17,14 @@ def test_pilatus_raises_error_if_fastchip_and_no_args_list(fake_caput):
 
 
 @patch("mx_bluesky.I24.serial.setup_beamline.setup_beamline.caput")
-def test_eiger_raises_error_if_quickshot_and_no_args_list(fake_caput):
+@patch("mx_bluesky.I24.serial.setup_beamline.setup_beamline.caget")
+def test_eiger_raises_error_if_quickshot_and_no_args_list(fake_caget, fake_caput):
     with pytest.raises(TypeError):
         setup_beamline.eiger("quickshot")
 
 
 @patch("mx_bluesky.I24.serial.setup_beamline.setup_beamline.caput")
-def test_zebra1_return_to_normal(fake_caput):
+@patch("mx_bluesky.I24.serial.setup_beamline.setup_beamline.caget")
+def test_zebra1_return_to_normal(fake_caget, fake_caput):
     setup_beamline.zebra1("return-to-normal")
     assert fake_caput.call_count == 20
