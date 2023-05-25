53a1b11bd70049014b6c14726506ae2abfefb808
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2023-05-25 13:54:42 +0100

    Maybe if I fix the right one

--- tests/I24/serial/setup_beamline/test_setup_beamline.py
+++ tests/I24/serial/setup_beamline/test_setup_beamline.py
@@ -11,7 +11,8 @@ def test_beamline_raises_error_if_quickshot_and_no_args_list():
 
 
 @patch("mx_bluesky.I24.serial.setup_beamline.setup_beamline.caput")
-def test_pilatus_raises_error_if_fastchip_and_no_args_list(fake_caput):
+@patch("mx_bluesky.I24.serial.setup_beamline.setup_beamline.caget")
+def test_pilatus_raises_error_if_fastchip_and_no_args_list(fake_caget, fake_caput):
     with pytest.raises(TypeError):
         setup_beamline.pilatus("fastchip")
 
@@ -24,7 +25,6 @@ def test_eiger_raises_error_if_quickshot_and_no_args_list(fake_caget, fake_caput
 
 
 @patch("mx_bluesky.I24.serial.setup_beamline.setup_beamline.caput")
-@patch("mx_bluesky.I24.serial.setup_beamline.setup_beamline.caget")
-def test_zebra1_return_to_normal(fake_caget, fake_caput):
+def test_zebra1_return_to_normal(fake_caput):
     setup_beamline.zebra1("return-to-normal")
     assert fake_caput.call_count == 20
