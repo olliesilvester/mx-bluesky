59296b8b3b1b22c18cf66c7f5fe0918fd3bc7228
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2023-05-26 13:58:53 +0100

    Write some tests around run_extruder24

--- tests/I24/serial/extruder/test_extruder_collect.py
+++ tests/I24/serial/extruder/test_extruder_collect.py
@@ -1,13 +1,14 @@
 from unittest.mock import mock_open, patch
 
-from mx_bluesky.I24.serial.extruder.i24ssx_Extruder_Collect_py3v2 import (  # run_extruderi24,
+from mx_bluesky.I24.serial.extruder.i24ssx_Extruder_Collect_py3v2 import (
     initialise_extruderi24,
     moveto,
+    run_extruderi24,
     scrape_parameter_file,
 )
 
 params_file_str = "visit foo \ndirectory bar \nfilename boh \nnum_imgs 1  \
-    \nexp_time 0.1 \ndet_dist 100 \ndet_type eig \npump_probe false \
+    \nexp_time 0.1 \ndet_dist 100 \ndet_type eiger \npump_probe false \
     \npump_exp 0 \npump_delay 0"
 
 
@@ -31,12 +32,21 @@ def test_initialise_extruder(fake_caget, fake_caput):
 
 @patch("mx_bluesky.I24.serial.extruder.i24ssx_Extruder_Collect_py3v2.caput")
 @patch("mx_bluesky.I24.serial.extruder.i24ssx_Extruder_Collect_py3v2.caget")
-def test_moveto(fake_caput, fake_caget):
+def test_moveto(fake_caget, fake_caput):
     moveto("enterhutch")
     assert fake_caget.call_count == 1
     assert fake_caput.call_count == 1
 
 
-# @patch()
-def test_run_extruder():
-    pass
+@patch(
+    "mx_bluesky.I24.serial.extruder.i24ssx_Extruder_Collect_py3v2.open",
+    mock_open(read_data=params_file_str),
+)
+@patch("mx_bluesky.I24.serial.extruder.i24ssx_Extruder_Collect_py3v2.DCID")
+@patch("mx_bluesky.I24.serial.extruder.i24ssx_Extruder_Collect_py3v2.call_nexgen")
+@patch("mx_bluesky.I24.serial.extruder.i24ssx_Extruder_Collect_py3v2.caput")
+@patch("mx_bluesky.I24.serial.extruder.i24ssx_Extruder_Collect_py3v2.caget")
+@patch("mx_bluesky.I24.serial.extruder.i24ssx_Extruder_Collect_py3v2.sup")
+def test_run_extruder(fake_sup, fake_caget, fake_caput, fake_nexgen, fake_dcid):
+    run_extruderi24()
+    assert fake_nexgen.call_count == 1
--- tests/I24/serial/setup_beamline/test_setup_beamline.py
+++ tests/I24/serial/setup_beamline/test_setup_beamline.py
@@ -5,9 +5,24 @@ import pytest
 from mx_bluesky.I24.serial.setup_beamline import setup_beamline
 
 
+@patch("mx_bluesky.I24.serial.setup_beamline.setup_beamline.caput")
+def test_beamline_collect(fake_caput):
+    setup_beamline.beamline("collect")
+    assert fake_caput.call_count == 4
+
+
 def test_beamline_raises_error_if_quickshot_and_no_args_list():
     with pytest.raises(TypeError):
         setup_beamline.beamline("quickshot")
+
+
+@patch("mx_bluesky.I24.serial.setup_beamline.setup_beamline.caput")
+@patch("mx_bluesky.I24.serial.setup_beamline.setup_beamline.caget")
+def test_beamline_quickshot(fake_caget, fake_caput):
+    fake_caget.return_value = 100
+    setup_beamline.beamline("quickshot", ["100"])
+    assert fake_caput.call_count == 1
+    assert fake_caget.call_count == 2
 
 
 @patch("mx_bluesky.I24.serial.setup_beamline.setup_beamline.caput")
@@ -25,6 +40,19 @@ def test_eiger_raises_error_if_quickshot_and_no_args_list(fake_caget, fake_caput
 
 
 @patch("mx_bluesky.I24.serial.setup_beamline.setup_beamline.caput")
+@patch("mx_bluesky.I24.serial.setup_beamline.setup_beamline.caget")
+def test_eiger_quickshot(fake_caget, fake_caput):
+    setup_beamline.eiger("quickshot", ["", "", "1", "0.1"])
+    assert fake_caput.call_count == 32
+
+
+@patch("mx_bluesky.I24.serial.setup_beamline.setup_beamline.caput")
 def test_zebra1_return_to_normal(fake_caput):
     setup_beamline.zebra1("return-to-normal")
     assert fake_caput.call_count == 20
+
+
+@patch("mx_bluesky.I24.serial.setup_beamline.setup_beamline.caput")
+def test_zebra1_quickshot(fake_caput):
+    setup_beamline.zebra1("quickshot", [0, 1])
+    assert fake_caput.call_count == 7
