65b926adba05f33a5a0b1ddf0a0a29b533cad959
noemifrisina <noemifrisina@users.noreply.github.com>
2023-05-30 14:46:21 +0000

    deploy: a27ab953ce28ad3cfe1afa17e531235bced89925

--- 14_I24-fix-linting/_static/documentation_options.js
+++ 14_I24-fix-linting/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev92+g0b7fe87',
+    VERSION: '0.1.dev93+ga27ab95',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
