dad4e59b13d17f84cfca3aa4c8ddc2a1ff0b1d56
noemifrisina <noemifrisina@users.noreply.github.com>
2023-06-07 07:59:16 +0000

    deploy: b0c8aa30aa2c16f13ad3a3baebd763aaddf0aa05

--- i24-get-detector/_static/documentation_options.js
+++ i24-get-detector/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev104+ga336177',
+    VERSION: '0.1.dev105+gb0c8aa3',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
