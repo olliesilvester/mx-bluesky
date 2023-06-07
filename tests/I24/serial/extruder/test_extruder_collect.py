fbc6b5162abe1eaafed13dba58e09cdde37cfbaa
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2023-06-07 09:59:17 +0100

    Fix hanging test

--- tests/I24/serial/extruder/test_extruder_collect.py
+++ tests/I24/serial/extruder/test_extruder_collect.py
@@ -6,6 +6,7 @@ from mx_bluesky.I24.serial.extruder.i24ssx_Extruder_Collect_py3v2 import (
     run_extruderi24,
     scrape_parameter_file,
 )
+from mx_bluesky.I24.serial.setup_beamline import Eiger
 
 params_file_str = """visit foo
 directory bar
@@ -30,7 +31,11 @@ def test_scrape_parameter_file():
 
 
 @patch("mx_bluesky.I24.serial.extruder.i24ssx_Extruder_Collect_py3v2.caput")
-def test_initialise_extruder(fake_caput):
+@patch(
+    "mx_bluesky.I24.serial.extruder.i24ssx_Extruder_Collect_py3v2.sup.get_detector_type"
+)
+def test_initialise_extruder(fake_det, fake_caput):
+    fake_det.return_value = Eiger()
     initialise_extruderi24()
     assert fake_caput.call_count == 11
 
