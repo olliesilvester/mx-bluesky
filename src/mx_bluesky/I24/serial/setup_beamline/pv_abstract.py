295f28880924caadbf4be209a630e16ee3317bf3
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2023-06-08 10:25:36 +0100

    Add pv_abstract for experiment types

--- src/mx_bluesky/I24/serial/setup_beamline/pv_abstract.py
+++ src/mx_bluesky/I24/serial/setup_beamline/pv_abstract.py
@@ -10,6 +10,8 @@ from typing import Union
 
 from mx_bluesky.I24.serial.setup_beamline import pv
 
+# Detectors
+
 
 class Pilatus:
     id = 58
@@ -63,4 +65,48 @@ class Eiger:
         beamy = pv.eiger_beamy
 
 
+# Experiment types
+
+
+class Extruder:
+    name = "extruder-collect"
+
+    class pv:
+        visit = pv.ioc12_gp1
+        directory = pv.ioc12_gp2
+        filename = pv.ioc12_gp3
+        exp_time = pv.ioc12_gp5
+        det_dist = pv.ioc12_gp7
+        det_type = pv.ioc12_gp15
+        pump_exp = pv.ioc12_gp9
+        pump_delay = pv.ioc12_gp10
+        spec_pv = {
+            "num_imgs": pv.ioc12_gp4,
+            "pump_status": pv.ioc12_gp6,  # if 1, true
+        }
+
+
+class FixedTarget:
+    name = "chip-collect"
+
+    class pv:
+        visit = pv.me14e_gp100
+        directory = pv.me14e_filepath
+        filename = pv.me14e_chip_name
+        exp_time = pv.me14e_exptime
+        det_dist = pv.me14e_dcdetdist
+        det_type = pv.me14e_gp101
+        pump_exp = pv.me14e_gp103
+        pump_delay = pv.me14e_gp110
+        spec_pv = {
+            "chip_type": pv.me14e_gp1,
+            "map_type": pv.me14e_gp2,
+            "n_exposures": pv.me14e_gp3,
+            "pump_repeat": pv.me14e_gp4,
+            "prepump_exp": pv.me14e_gp109,
+        }
+
+
+#
 Detector = Union[Pilatus, Eiger]
+ExperimentType = Union[Extruder, FixedTarget]
