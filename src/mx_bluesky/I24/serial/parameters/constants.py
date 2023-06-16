ec596e20855d1440ff751ddb7337e33763c53883
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2023-06-16 12:00:54 +0100

    Actually add the file with the patchs

---
+++ src/mx_bluesky/I24/serial/parameters/constants.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+PARAM_FILE_PATH_EX = Path(
+    "src/mx_bluesky/I24/serial/parameters/extruder"
+)  # /dls_sw/i24/scripts/extruder/
+PARAM_FILE_PATH_FT = Path("src/mx_bluesky/I24/serial/parameters/fixed_target")
+LITEMAP_PATH = Path("src/mx_bluesky/I24/serial/parameters/fixed_target/litemaps")
+FULLMAP_PATH = Path("src/mx_bluesky/I24/serial/parameters/fixed_target/litemaps")
