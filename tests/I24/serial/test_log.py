0ec69d4ef9110d07edcacde82cce59f14724b24f
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2023-06-19 22:54:00 +0100

    Add a test for logging

--- tests/I24/serial/test_log.py
+++ tests/I24/serial/test_log.py
@@ -1,7 +1,15 @@
 import logging
 from unittest.mock import patch
 
+import pytest
+
 from mx_bluesky.I24.serial import log
+
+
+@pytest.fixture
+def dummy_logger():
+    logger = logging.getLogger("I24ssx")
+    yield logger
 
 
 @patch("mx_bluesky.I24.serial.log.Path.mkdir")
@@ -11,7 +19,17 @@ def test_logging_file_path(mock_dir):
     assert log_path.as_posix() == "tmp/logs"
 
 
-def test_basic_logging_config():
-    logger = logging.getLogger("I24ssx")
-    assert logger.hasHandlers() is True
-    assert len(logger.handlers) == 1
+def test_basic_logging_config(dummy_logger):
+    assert dummy_logger.hasHandlers() is True
+    assert len(dummy_logger.handlers) == 1
+    assert dummy_logger.handlers[0].level == logging.DEBUG
+
+
+@patch("mx_bluesky.I24.serial.log.Path.mkdir")
+def test_logging_config_with_filehandler(mock_dir, dummy_logger):
+    log.config("dummy.log", delayed=True)
+    assert len(dummy_logger.handlers) == 2
+    assert mock_dir.call_count == 1
+    assert dummy_logger.handlers[1].level == logging.DEBUG
+    # Clear FileHandler to avoid other tests failing if it is kept open
+    dummy_logger.removeHandler(dummy_logger.handlers[1])
