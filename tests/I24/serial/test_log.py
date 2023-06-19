9876d89f8d8659d98e9734c97ac5d887723db10c
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2023-06-19 19:15:26 +0100

    Add small test of basic logging config

--- tests/I24/serial/test_log.py
+++ tests/I24/serial/test_log.py
@@ -1,3 +1,4 @@
+import logging
 from unittest.mock import patch
 
 from mx_bluesky.I24.serial import log
@@ -8,3 +9,9 @@ def test_logging_file_path(mock_dir):
     log_path = log._get_logging_file_path()
     assert mock_dir.call_count == 1
     assert log_path.as_posix() == "tmp/logs"
+
+
+def test_basic_logging_config():
+    logger = logging.getLogger("I24ssx")
+    assert logger.hasHandlers() is True
+    assert len(logger.handlers) == 1
