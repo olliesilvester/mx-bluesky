737af07ef1ef929fc0f9ce4fa8ea900a28f5472d
noemifrisina <noemifrisina@users.noreply.github.com>
2023-06-19 22:27:36 +0000

    deploy: dd01a301ea53f5eb2710c053c46c57f8da1501a4

--- 14_tidy-up-i24-ssx/_static/documentation_options.js
+++ 14_tidy-up-i24-ssx/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev147+g0ec69d4',
+    VERSION: '0.1.dev148+gdd01a30',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
