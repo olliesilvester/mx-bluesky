84c29273267a2a5a73dbbc27eab16aa0f86a65ff
noemifrisina <noemifrisina@users.noreply.github.com>
2023-06-22 10:54:26 +0000

    deploy: 11890d3d11d447abac5232c84aad5a781a6639ab

--- 19_remove-sacla/_static/documentation_options.js
+++ 19_remove-sacla/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev160+gd2ca7f7',
+    VERSION: '0.1.dev161+g11890d3',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
