5e35ad56a027f17422638ba262e10d1cbdc941eb
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2023-06-23 10:56:13 +0100

    Remove old instance of smaract vertical goniometer and use the new one in dodal

--- src/mx_bluesky/I24/exercise_smaract_motors.py
+++ src/mx_bluesky/I24/exercise_smaract_motors.py
@@ -4,17 +4,16 @@ from collections import namedtuple
 
 import bluesky.plan_stubs as bps
 from bluesky import RunEngine
-
-from .smaract import SmarAct
+from dodal.devices.i24.i24_vgonio import VGonio
 
 Point3D = namedtuple("Point3D", ("x", "y", "z"))
 
 
-def move_xz_motors(smaract_motors: SmarAct, pos):
+def move_xz_motors(smaract_motors: VGonio, pos):
     yield from bps.mv()
 
 
-def move_omega(smaract_motors: SmarAct):
+def move_omega(smaract_motors: VGonio):
     yield from bps.mv(smaract_motors.omega, 45)
 
 
