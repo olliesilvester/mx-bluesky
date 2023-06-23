a262fe040214ddd2f4690514f19a4cfc9a0afa24
Tom-Willemsen <Tom-Willemsen@users.noreply.github.com>
2023-06-23 09:18:52 +0000

    deploy: 9d762c52fcae28bc9d33c8b806146895123f874f

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev157+g5233f07',
+    VERSION: '0.1.dev166+g9d762c5',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
