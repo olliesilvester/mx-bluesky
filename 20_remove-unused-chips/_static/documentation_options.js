6c5459ac83a9ac2ac649a51c909cdcd3a961f333
noemifrisina <noemifrisina@users.noreply.github.com>
2023-07-11 16:31:17 +0000

    deploy: 3d476b8aed5bf5994cb12a6898bb3544ef3828a9

--- 20_remove-unused-chips/_static/documentation_options.js
+++ 20_remove-unused-chips/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev177+g90ab4d8',
+    VERSION: '0.1.dev178+g3d476b8',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
