a48c94ae731638b06a541d86efa5673c4de256d4
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2023-07-13 11:03:10 +0100

    Add dodal dependency, install it in venv and set up workspace

--- dls_dev_setup.sh
+++ dls_dev_setup.sh
@@ -8,6 +8,15 @@ mkdir .venv
 
 python -m venv .venv
 source .venv/bin/activate
+
 pip install -e .[dev]
+
+# Ensure we use a local version of dodal
+if [ ! -d "../dodal" ]; then
+  git clone git@github.com:DiamondLightSource/dodal.git ../dodal
+fi
+
+pip uninstall -y dodal
+pip install -e ../dodal[dev]
 
 tox -p
