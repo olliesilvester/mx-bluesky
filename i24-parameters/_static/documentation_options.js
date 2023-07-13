fa5b88ae2315a55f77a3cec7d177c94473b952f7
noemifrisina <noemifrisina@users.noreply.github.com>
2023-07-13 11:16:27 +0000

    deploy: c8c9830372afde39fe71257c2adde09609ed317c

--- i24-parameters/_static/documentation_options.js
+++ i24-parameters/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev178+g4a148a3',
+    VERSION: '0.1.dev179+gc8c9830',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
