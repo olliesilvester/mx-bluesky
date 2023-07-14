ea6bc3c67b2e5142f6bcd7293971ae98f3975f42
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2023-07-14 17:34:25 +0100

    Temporarily remove dodal

--- src/mx_bluesky/I24/exercise_smaract_motors.py
+++ src/mx_bluesky/I24/exercise_smaract_motors.py
@@ -2,19 +2,20 @@ from __future__ import annotations
 
 from collections import namedtuple
 
-import bluesky.plan_stubs as bps
+# import bluesky.plan_stubs as bps
 from bluesky import RunEngine
-from dodal.devices.i24.i24_vgonio import VGonio
+
+# from dodal.devices.i24.i24_vgonio import VGonio
 
 Point3D = namedtuple("Point3D", ("x", "y", "z"))
 
 
-def move_xz_motors(smaract_motors: VGonio, pos):
-    yield from bps.mv()
+# def move_xz_motors(smaract_motors: VGonio, pos):
+#    yield from bps.mv()
 
 
-def move_omega(smaract_motors: VGonio):
-    yield from bps.mv(smaract_motors.omega, 45)
+# def move_omega(smaract_motors: VGonio):
+#    yield from bps.mv(smaract_motors.omega, 45)
 
 
 def exercise_plan():
