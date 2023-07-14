19fdc6ad96202ebd59a69470552daac6637e4f26
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2023-07-14 19:14:23 +0100

    Add import

--- src/mx_bluesky/I24/serial/parameters/__init__.py
+++ src/mx_bluesky/I24/serial/parameters/__init__.py
@@ -1,5 +1,7 @@
-from .constants import SSXType
-from .experiment_parameters import ExperimentParameters
+from __future__ import annotations
+
+from mx_bluesky.I24.serial.parameters.constants import SSXType
+from mx_bluesky.I24.serial.parameters.experiment_parameters import ExperimentParameters
 
 __all__ = [
     "SSXType",
