8457ad92b3f4d2afa14354ea71fab131a16af5b9
noemifrisina <noemifrisina@users.noreply.github.com>
2023-07-14 17:45:21 +0000

    deploy: cfd39c4ed6daa055e4c671305635b29ec094dedd

--- add-dodal/_static/documentation_options.js
+++ add-dodal/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev192+g6a7bfde',
+    VERSION: '0.1.dev193+gcfd39c4',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
