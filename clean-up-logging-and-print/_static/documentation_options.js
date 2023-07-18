24daf1aef6d4ead14668f23740bea63724fd5fe0
noemifrisina <noemifrisina@users.noreply.github.com>
2023-07-18 14:48:03 +0000

    deploy: b09f51a00b7d6f071eb463904d4d35b1abafd867

--- clean-up-logging-and-print/_static/documentation_options.js
+++ clean-up-logging-and-print/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev213+gb04f338',
+    VERSION: '0.1.dev214+gb09f51a',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
