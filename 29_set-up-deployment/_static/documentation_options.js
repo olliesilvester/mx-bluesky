5939eef627ae5458e309aa596b78af2557081d1f
noemifrisina <noemifrisina@users.noreply.github.com>
2023-07-25 09:17:20 +0000

    deploy: 0ef5eacf75b8ff6000166b65c82547559d5992bd

--- 29_set-up-deployment/_static/documentation_options.js
+++ 29_set-up-deployment/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev233+g89dc6f7',
+    VERSION: '0.1.dev234+g0ef5eac',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
