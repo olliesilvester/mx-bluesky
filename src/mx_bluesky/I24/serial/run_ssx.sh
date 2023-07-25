6f5d29637d3c55c81fbff00282d2755896e6e2c7
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2023-07-25 13:25:42 +0100

    Fix paths in bash script

--- src/mx_bluesky/I24/serial/run_ssx.sh
+++ src/mx_bluesky/I24/serial/run_ssx.sh
@@ -3,19 +3,23 @@
 # Start the edm screen relative to requested serial collection
 expt_type=${1:-FT}
 
+current=$( realpath "$( dirname "$0" )" )
+cd $current
+cd ../../../..
+
 echo "Activating python environment..."
-source /dls_sw/i24/software/bluesky/mx_bluesky/.venv/bin/activate
+source .venv/bin/activate
 
 shopt -s nocasematch
 
 if [[ $expt_type == "FT" ]] || [[ $expt_type == "fixed-target" ]]
 then
     echo "Starting fixed target edm screen."
-    edm /dls_sw/i24/software/bluesky/mx_bluesky/src/mx_bluesky/I24/serial/fixed_target/FT-gui-edm/DiamondChipI24-py3v1.edl
+    edm ./edm_serial/FT-gui/DiamondChipI24-py3v1.edl
 elif [[ $expt_type == "EX" ]] || [[ $expt_type == "extruder" ]]
 then
     echo "Starting extruder edm screen."
-    edm /dls_sw/i24/software/bluesky/mx_bluesky/src/mx_bluesky/I24/serial/extruder/EX-gui-edm/DiamondExtruder-I24-py3v1.edl
+    edm ./edm_serial/EX-gui/DiamondExtruder-I24-py3v1.edl
 else
     echo "No edm found for $expt_type."
 fi
