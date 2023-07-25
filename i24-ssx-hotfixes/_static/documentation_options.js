0936d8ef65b1fbfb8cd1fc9799da43fb14850ba3
noemifrisina <noemifrisina@users.noreply.github.com>
2023-07-25 15:44:56 +0000

    deploy: 8b5714e6a85e33e20c0b36aa71676a780a531729

--- i24-ssx-hotfixes/_static/documentation_options.js
+++ i24-ssx-hotfixes/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev252+g77aa024',
+    VERSION: '0.1.dev253+g8b5714e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
