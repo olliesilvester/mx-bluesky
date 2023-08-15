e79e6a3de2db6d26dc4f3effb37e7c1e8a3e68c1
noemifrisina <noemifrisina@users.noreply.github.com>
2023-08-15 13:05:22 +0000

    deploy: c829f3ddc515c4f810d8abe70ebc79cf514f56e6

--- 11_I24-exercise-motors/_static/documentation_options.js
+++ 11_I24-exercise-motors/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev215+g96b158d',
+    VERSION: '0.1.dev266+gc829f3d',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
