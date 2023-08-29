212d976136bf594b1fef8c13a0d2ffe3f777f33d
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2023-08-29 10:18:03 +0100

    Print an error message if file does not exist

--- src/mx_bluesky/I24/serial/set_visit_directory.sh
+++ src/mx_bluesky/I24/serial/set_visit_directory.sh
@@ -2,10 +2,17 @@
 
 # Set visit directory for current experiment type from values stored in a file
 filename="/dls_sw/i24/etc/ssx_current_visit.txt"
+
+if [[ ! -f "$filename" ]]; then
+    echo "The file $filename does not exist. Impossible to set the visit directory."
+    exit 1
+fi
+
+
 echo "Reading file: $filename"
 
 visit=$(sed -n '1p' $filename)
-expt_type=$(sed -n '2p' $filename)
+expt_type=${1:-FT}
 
 ex_pv=BL24I-EA-IOC-12:GP1
 ft_pv=ME14E-MO-IOC-01:GP100
@@ -24,4 +31,5 @@ then
     echo "Visit set to: $visit"
 else
     echo -e "Unknown experiment type, visit PV not set. \nValid experiment values: fixed-target, extruder."
+    exit 1
 fi
