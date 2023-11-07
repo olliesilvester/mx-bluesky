8d5e51f422d1347071cc5b10d95ae77ec078f52e
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2023-11-07 13:21:53 +0000

    Add to onmouse test

--- tests/I24/serial/fixed_target/test_moveonclick.py
+++ tests/I24/serial/fixed_target/test_moveonclick.py
@@ -18,16 +18,31 @@ def fake_oav() -> OAV:
     return i24.oav(fake_with_ophyd_sim=True)
 
 
+@pytest.mark.parametrize(
+    "beam_position, expected_1J, expected_2J",
+    [
+        ((15, 10), "#1J:-90", "#2J:60"),
+        ((100, 150), "#1J:-600", "#2J:900"),
+        ((475, 309), "#1J:-2850", "#2J:1854"),
+        ((638, 392), "#1J:-3828", "#2J:2352"),
+    ],
+)
 @patch("mx_bluesky.I24.serial.fixed_target.i24ssx_moveonclick.caput")
 @patch("mx_bluesky.I24.serial.fixed_target.i24ssx_moveonclick.get_beam_centre_from_oav")
-def test_onMouse__gets_beam_position_and_sends_correct_str(fake_beam_pos, fake_caput):
-    fake_beam_pos.side_effect = [(15, 10)]
+def test_onMouse_gets_beam_position_and_sends_correct_str(
+    fake_get_beam_pos,
+    fake_caput,
+    beam_position,
+    expected_1J,
+    expected_2J,
+):
+    fake_get_beam_pos.side_effect = [beam_position]
     onMouse(cv.EVENT_LBUTTONUP, 0, 0, "", "")
     assert fake_caput.call_count == 2
     fake_caput.assert_has_calls(
         [
-            call(ANY, "#1J:-90"),
-            call(ANY, "#2J:60"),
+            call(ANY, expected_1J),
+            call(ANY, expected_2J),
         ]
     )
 
