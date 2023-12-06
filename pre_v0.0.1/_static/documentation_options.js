ce7ab5144cc03be63109ad2ab522af29f304925b
noemifrisina <noemifrisina@users.noreply.github.com>
2023-12-06 13:23:59 +0000

    deploy: 5fc3ef49c08f3354bc3f56dae52efcbc544054cc

---
+++ pre_v0.0.1/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.1.dev381+g5fc3ef4',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
