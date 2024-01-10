d09726d42201485ca46fd1911537c43971dac9cc
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2024-01-10 16:08:50 +0000

    Possible fix for parameter file paths

--- src/mx_bluesky/I24/serial/parameters/constants.py
+++ src/mx_bluesky/I24/serial/parameters/constants.py
@@ -14,26 +14,11 @@ OAV_CONFIG_FILES = {
 }
 OAV1_CAM = "http://bl24i-di-serv-01.diamond.ac.uk:8080/OAV1.mjpg.mjpg"
 
-PARAM_FILE_PATH = Path("src/mx_bluesky/I24/serial/parameters").expanduser().resolve()
-PARAM_FILE_PATH_FT = (
-    Path("src/mx_bluesky/I24/serial/parameters/fixed_target").expanduser().resolve()
-)
-LITEMAP_PATH = (
-    Path("src/mx_bluesky/I24/serial/parameters/fixed_target/litemaps")
-    .expanduser()
-    .resolve()
-)
-FULLMAP_PATH = (
-    Path("src/mx_bluesky/I24/serial/parameters/fixed_target/fullmaps")
-    .expanduser()
-    .resolve()
-)
-PVAR_FILE_PATH = (
-    Path("src/mx_bluesky/I24/serial/parameters/fixed_target/pvar_files")
-    .expanduser()
-    .resolve()
-)
 HEADER_FILES_PATH = Path("/dls_sw/i24/scripts/fastchips/").expanduser().resolve()
-CS_FILES_PATH = (
-    Path("src/mx_bluesky/I24/serial/parameters/fixed_target/cs").expanduser().resolve()
-)
+
+PARAM_FILE_PATH = Path(__file__).absolute()
+PARAM_FILE_PATH_FT = PARAM_FILE_PATH / "fixed_target"
+LITEMAP_PATH = PARAM_FILE_PATH / "fixed_target/litemaps"
+FULLMAP_PATH = PARAM_FILE_PATH / "fixed_target/fullmaps"
+PVAR_FILE_PATH = PARAM_FILE_PATH / "fixed_target/pvar_files"
+CS_FILES_PATH = PARAM_FILE_PATH / "fixed_target/cs"
