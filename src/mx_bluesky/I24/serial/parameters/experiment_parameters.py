da0c66ad0a4834dfa82a1e0a46d08ac972e043b1
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2024-03-22 16:22:07 +0000

    Small model to start with

--- src/mx_bluesky/I24/serial/parameters/experiment_parameters.py
+++ src/mx_bluesky/I24/serial/parameters/experiment_parameters.py
@@ -1,4 +1,6 @@
-from pydantic import BaseModel
+from typing import Optional
+
+from pydantic import BaseModel, ConfigDict, validator
 
 from mx_bluesky.I24.serial.fixed_target.ft_utils import (
     ChipType,
@@ -7,23 +9,53 @@ from mx_bluesky.I24.serial.fixed_target.ft_utils import (
 )
 
 
-class SerialExptParameters(BaseModel):
+class SerialExperiment(BaseModel):
     visit: str
     directory: str
     filename: str
     exposure_time_s: float
     detector_distance_mm: float
-    detector_type: str
+    detector_name: str
+
+
+class LaserExperiment(BaseModel):
+    laser_dwell_s: Optional[float] = None  # pump exposure time
+    laser_delay_s: Optional[float] = None  # pump delay
+    pre_pump_exposure_s: Optional[float] = None  # Pre illumination, just for chip
+
+
+class ExtruderParameters(SerialExperiment, LaserExperiment):
+    num_images: int
     pump_status: bool
 
 
-class ExtruderParameters(SerialExptParameters):
-    num_images: int
-    pump_exposure_s: float
-    pump_delay_s: float
-
-
-class FixedTargetParameters(SerialExptParameters):
+class FixedTargetParameters(SerialExperiment, LaserExperiment):
+    model_config = ConfigDict(use_enum_values=True)
+    num_exposures: int
     chip_type: ChipType
     map_type: MappingType
     pump_repeat: PumpProbeSetting
+    checker_pattern: bool = False
+
+    @validator("chip_type", pre=True)
+    def _parse_chip(cls, chip_type: str | int):
+        if isinstance(chip_type, str):
+            return ChipType[chip_type]
+        else:
+            return ChipType(chip_type)
+
+    @validator("map_type", pre=True)
+    def _parse_map(cls, map_type: str | int):
+        if isinstance(map_type, str):
+            return MappingType[map_type]
+        else:
+            return MappingType(map_type)
+
+    def get_approx_chip_size(self) -> float:
+        """Returns an approximation of the chip size for the move during alignment \
+            of the fiducials
+        """
+        if str(self.chip_type) == "OxfordInner":
+            return 24.60
+        else:
+            return 25.40
