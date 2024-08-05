c8ef9a047c54951c43b42e821fcc0afade7bddae
Oliver Silvester <oliver.silvester@diamond.ac.uk>
2024-08-05 14:41:42 +0100

    Create general do_fgs plan stub with basic tests

--- src/mx_bluesky/plans/level_1/do_fgs.py
+++ src/mx_bluesky/plans/level_1/do_fgs.py
@@ -1,36 +1,33 @@
-from typing import Callable
+from time import time
+from typing import Callable, Optional
 
 import bluesky.plan_stubs as bps
 from dodal.devices.eiger import EigerDetector
-from dodal.devices.fast_grid_scan import FastGridScanCommon as GridScanDevice
+from dodal.devices.fast_grid_scan import FastGridScanCommon
 from dodal.devices.synchrotron import Synchrotron
 from dodal.log import LOGGER
 from dodal.plans.check_topup import check_topup_and_wait_if_necessary
 
-"""
-do fgs should:
-Accept a gridscan device, a detector device, a synchrotron device, a group of optional parameters, metadata
-"""
 
-#When other detectors are available in dodal, move the detector to a more general type
-def do_fgs(grid_scan_device: GridScanDevice, detector: EigerDetector, synchrotron: Synchrotron,
-           pre_plans: None | Callable, post_plans: None | Callable
-           ):
-    """Triggers a grid scan motion program and waits for completion. Optionally run other plans before and after completion.
-
-    The core logic for this lowest-level plan is to wait for synchrotron top-up, then kickoff and wait for FGS completion.
+def do_fgs(
+    grid_scan_device: FastGridScanCommon,
+    detector: EigerDetector,  # Once Eiger inherits from StandardDetector, use that type instead
+    synchrotron: Synchrotron,
+    pre_plans: Optional[Callable] = None,
+    post_plans: Optional[Callable] = None,
+):
+    """Triggers a grid scan motion program and waits for completion, accounting for synchrotron topup.
+        Optionally run other plans before and after completion. A bluesky run MUST be open before this plan is
+        called
 
     Args:
         grid_scan_device (GridScanDevice): Device which can trigger a fast grid scan and wait for completion
-        detector (StandardDetector): Need to convert Eiger to standard detector / something more generic
+        detector (EigerDetector)
         synchrotron (Synchrotron): Synchrotron device
         pre_plans (Optional, Callable): Generic plan called just before kickoff, eg zocalo setup. Parameters specified in higher-level plans.
         post_plans (Optional, Callable): Generic plan called just before complete, eg waiting on zocalo.
-
-
-    Returns:
-    Status
     """
+
     expected_images = yield from bps.rd(grid_scan_device.expected_images)
     exposure_sec_per_image = yield from bps.rd(detector.cam.acquire_time)
     LOGGER.info("waiting for topup if necessary...")
@@ -45,7 +42,12 @@ def do_fgs(grid_scan_device: GridScanDevice, detector: EigerDetector, synchrotro
     yield from bps.wait()
     LOGGER.info("kicking off FGS")
     yield from bps.kickoff(grid_scan_device, wait=True)
+    gridscan_start_time = time()
     if post_plans:
         yield from post_plans()
     LOGGER.info("completing FGS")
     yield from bps.complete(grid_scan_device, wait=True)
+    # Remove this logging statement once metrics have been added
+    LOGGER.info(
+        f"Gridscan motion program took {round(time()-gridscan_start_time,2)} to complete"
+    )
---
+++ tests/plans/level_1/test_do_fgs.py
@@ -0,0 +1,81 @@
+from unittest.mock import MagicMock, patch
+
+import pytest
+from bluesky import plan_stubs as bps
+from bluesky.plan_stubs import null
+from bluesky.run_engine import RunEngine
+from bluesky.simulators import RunEngineSimulator
+from dodal.beamlines.i03 import eiger
+from dodal.devices.fast_grid_scan import ZebraFastGridScan
+from dodal.devices.synchrotron import Synchrotron, SynchrotronMode
+from ophyd_async.core import DeviceCollector, set_mock_value
+
+from mx_bluesky.plans.level_1.do_fgs import do_fgs
+
+
+@pytest.fixture
+def fgs_devices(RE):
+    with DeviceCollector(mock=True):
+        synchrotron = Synchrotron()
+        grid_scan_device = ZebraFastGridScan("zebra_fgs")
+
+    # Eiger done separately as not ophyd-async yet
+    detector = eiger(fake_with_ophyd_sim=True)
+
+    return {
+        "synchrotron": synchrotron,
+        "grid_scan_device": grid_scan_device,
+        "detector": detector,
+    }
+
+
+def test_pre_and_post_plans_called_optionally(
+    sim_run_engine: RunEngineSimulator, fgs_devices
+):
+    def null_plan():
+        yield from null()
+
+    synchrotron = fgs_devices["synchrotron"]
+    detector = fgs_devices["detector"]
+    fgs_device = fgs_devices["grid_scan_device"]
+    post_plan = MagicMock(side_effect=null_plan)
+    pre_plan = MagicMock(side_effect=null_plan)
+    with patch("mx_bluesky.plans.level_1.do_fgs.check_topup_and_wait_if_necessary"):
+        msgs = sim_run_engine.simulate_plan(
+            do_fgs(
+                fgs_device,
+                detector,
+                synchrotron,
+                post_plans=post_plan,
+                pre_plans=pre_plan,
+            )
+        )
+        null_messages = [msg for msg in msgs if msg.command == "null"]
+        assert len(null_messages) == 2
+    with patch("mx_bluesky.plans.level_1.do_fgs.check_topup_and_wait_if_necessary"):
+        msgs = sim_run_engine.simulate_plan(
+            do_fgs(
+                fgs_device,
+                detector,
+                synchrotron,
+            )
+        )
+        null_messages = [msg for msg in msgs if msg.command == "null"]
+        assert len(null_messages) == 0
+
+
+def test_do_fgs_with_run_engine(RE: RunEngine, fgs_devices):
+    synchrotron = fgs_devices["synchrotron"]
+    set_mock_value(synchrotron.synchrotron_mode, SynchrotronMode.DEV)
+    detector = fgs_devices["detector"]
+    fgs_device: ZebraFastGridScan = fgs_devices["grid_scan_device"]
+
+    def do_fgs_plan_in_run():
+        yield from bps.open_run()
+        yield from do_fgs(fgs_device, detector, synchrotron)
+        yield from bps.close_run()
+
+    set_mock_value(fgs_device.status, 1)
+
+    with patch("mx_bluesky.plans.level_1.do_fgs.bps.complete"):
+        RE(do_fgs_plan_in_run())
