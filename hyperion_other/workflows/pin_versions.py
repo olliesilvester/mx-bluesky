c75a6e5abd553bc935dfcbc98279fd58ac50aa68
Oliver Silvester <oliver.silvester@diamond.ac.uk>
2024-08-28 15:16:52 +0100

    Get pre_release workflow working with pyproject

--- hyperion_other/workflows/pin_versions.py
+++ hyperion_other/workflows/pin_versions.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 import argparse
 import locale
 import os
@@ -7,17 +6,44 @@ import subprocess
 from functools import partial
 from sys import stderr, stdout
 
-SETUP_CFG_PATTERN = re.compile("(.*?\\S)\\s*(@(.*))?$")
-SETUP_UNPINNED_PATTERN = re.compile("(.*?\\S)\\s*([<>=]+(.*))?$")
+import requests
+
+PYPROJECT_TOML_PATTERN = re.compile("(.*?\\S)\\s*(@(.*))?$")
+PYPROJECT_UNPINNED_PATTERN = re.compile("(.*?\\S)\\s*([<>=]+(.*))?$")
 PIP = "pip"
 
 
 def rename_original(suffix):
-    os.rename("setup.cfg", "setup.cfg" + suffix)
+    os.rename("pyproject.toml", "pyproject.toml" + suffix)
 
 
 def normalize(package_name: str):
-    return re.sub(r"[-_.]+", "-", package_name).lower()
+    # Replace underscore and dots with hyphen, and convert to lower case
+    package_name = re.sub(r"[-_.]+", "-", package_name).lower()
+
+    package_name = package_name.replace('"', "")
+    package_name = package_name.replace(",", "")
+
+    # Remove square brackets in pip freeze, eg "fastapi[all]"" to "fastapi"
+    package_name = re.sub(r"\[.*\]$", "", package_name)
+    print(package_name)
+    return package_name
+
+
+# Returns latest version of dodal by looking at most recent tag on github
+def _get_dodal_version() -> str:
+    url = "https://api.github.com/repos/DiamondLightSource/dodal/tags"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        tags = response.json()
+        if tags:
+            return str(tags[0]["name"])
+        else:
+            stderr.write("Unable to find latest dodal tag")
+
+    stderr.write("Unable to find dodal on github")
+    raise Exception("Error finding the latest dodal version")
 
 
 def fetch_pin_versions() -> dict[str, str]:
@@ -29,9 +55,18 @@ def fetch_pin_versions() -> dict[str, str]:
         for line in lines:
             kvpair = line.split("==")
             if len(kvpair) != 2:
-                stderr.write(f"Unable to parse {line} - ignored\n")
+                # mx_bluesky will appear as '-e git+ssh://git@github.com/DiamondLightSource/mx-bluesky',
+                # and same for dodal unless it has manually been pinned beforehand
+                if line and not ("dls_dodal" in line or "mx_bluesky" in line):
+                    stderr.write(
+                        f"Unable to parse {line} - make sure this dependancy isn't pinned to a git hash\n"
+                    )
             else:
                 pin_versions[normalize(kvpair[0]).strip()] = kvpair[1].strip()
+
+        # Handle dodal separately to save us from having to manually set it
+        pin_versions["dls-dodal"] = _get_dodal_version()
+
         return pin_versions
     else:
         stderr.write(f"pip freeze failed with error code {process.returncode}\n")
@@ -41,12 +76,14 @@ def fetch_pin_versions() -> dict[str, str]:
 
 def run_pip_freeze():
     process = subprocess.run(
-        [PIP, "freeze"], capture_output=True, encoding=locale.getpreferredencoding()
+        [PIP, "list --format=freeze"],
+        capture_output=True,
+        encoding=locale.getpreferredencoding(),
     )
     return process
 
 
-def process_setup_cfg(input_fname, output_fname, dependency_processor):
+def process_pyproject_toml(input_fname, output_fname, dependency_processor):
     with open(input_fname) as input_file:
         with open(output_fname, "w") as output_file:
             process_files(input_file, output_file, dependency_processor)
@@ -55,9 +92,9 @@ def process_setup_cfg(input_fname, output_fname, dependency_processor):
 def process_files(input_file, output_file, dependency_processor):
     while line := input_file.readline():
         output_file.write(line)
-        if line.startswith("install_requires"):
+        if line.startswith("dependencies"):
             break
-    while (line := input_file.readline()) and not line.startswith("["):
+    while (line := input_file.readline()) and not line.startswith("]"):
         if line.isspace():
             output_file.write(line)
         else:
@@ -79,9 +116,9 @@ def write_with_comment(comment, text, output_file):
     output_file.write("\n")
 
 
-def update_setup_cfg_line(version_map: dict[str, str], line, output_file):
+def update_pyproject_toml_line(version_map: dict[str, str], line, output_file):
     stripped_line, comment = strip_comment(line)
-    if match := SETUP_UNPINNED_PATTERN.match(stripped_line):
+    if match := PYPROJECT_UNPINNED_PATTERN.match(stripped_line):
         normalized_name = normalize(match[1].strip())
         if normalized_name not in version_map:
             stderr.write(
@@ -91,7 +128,7 @@ def update_setup_cfg_line(version_map: dict[str, str], line, output_file):
 
         write_with_comment(
             comment,
-            f"    {normalized_name} == {version_map[normalized_name]}",
+            f'    "{normalized_name} == {version_map[normalized_name]}",',
             output_file,
         )
     else:
@@ -105,32 +142,34 @@ def write_commit_message(pinned_versions: dict[str, str]):
 
 def unpin_versions(line, output_file):
     stripped_line, comment = strip_comment(line)
-    if match := SETUP_CFG_PATTERN.match(stripped_line):
+    if match := PYPROJECT_TOML_PATTERN.match(stripped_line):
         if match[3] and match[3].strip().startswith("git+"):
-            write_with_comment(comment, match[1], output_file)
+            write_with_comment(comment, match[1] + '",', output_file)
             return
 
     output_file.write(line)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Pin dependency versions in setup.cfg")
+    parser = argparse.ArgumentParser(
+        description="Pin dependency versions in pyproject.toml"
+    )
     parser.add_argument(
         "--unpin",
-        help="remove pinned hashes from setup.cfg prior to pip installing latest",
+        help="remove pinned hashes from pyproject.toml prior to pip installing latest",
         action="store_true",
     )
     args = parser.parse_args()
 
     if args.unpin:
         rename_original(".orig")
-        process_setup_cfg("setup.cfg.orig", "setup.cfg", unpin_versions)
+        process_pyproject_toml("pyproject.toml.orig", "pyproject.toml", unpin_versions)
     else:
         rename_original(".unpinned")
         installed_versions = fetch_pin_versions()
-        process_setup_cfg(
-            "setup.cfg.unpinned",
-            "setup.cfg",
-            partial(update_setup_cfg_line, installed_versions),
+        process_pyproject_toml(
+            "pyproject.toml.unpinned",
+            "pyproject.toml",
+            partial(update_pyproject_toml_line, installed_versions),
         )
         write_commit_message(installed_versions)
--- hyperion_other/workflows/test_pin_versions.py
+++ hyperion_other/workflows/test_pin_versions.py
@@ -9,7 +9,7 @@ import pytest
 @pytest.fixture
 def patched_run_pip_freeze():
     with patch("pin_versions.run_pip_freeze") as run_pip_freeze:
-        with open("test_data/pip_freeze.txt") as freeze_output:
+        with open("hyperion_other/workflows/test_data/pip_freeze.txt") as freeze_output:
             mock_process = MagicMock()
             run_pip_freeze.return_value = mock_process
             mock_process.stdout = freeze_output.read()
@@ -40,6 +40,8 @@ def test_strip_comment(input, expected):
         ("dls-dodal", "dls-dodal"),
         ("dls_dodal", "dls-dodal"),
         ("dls.dodal", "dls-dodal"),
+        ("dls.dodal[all]", "dls-dodal"),
+        ('"dls.dodal",', "dls-dodal"),
     ],
 )
 def test_normalize(input, expected):
@@ -48,32 +50,43 @@ def test_normalize(input, expected):
 
 def test_unpin():
     with io.StringIO() as output_file:
-        with open("test_data/setup.cfg") as input_file:
+        with open("hyperion_other/workflows/test_data/pyproject.toml") as input_file:
             pin_versions.process_files(
                 input_file, output_file, pin_versions.unpin_versions
             )
-        with open("test_data/setup.cfg.unpinned") as expected_file:
+        with open(
+            "hyperion_other/workflows/test_data/pyproject.toml.unpinned"
+        ) as expected_file:
             assert output_file.getvalue() == expected_file.read()
 
 
+def test_get_dodal_version():
+    pass
+
+
+# TODO patch get dodal version
 @patch("pin_versions.stdout")
 def test_write_commit_message(mock_stdout, patched_run_pip_freeze):
     installed_versions = pin_versions.fetch_pin_versions()
     pin_versions.write_commit_message(installed_versions)
     mock_stdout.write.assert_called_once_with(
-        "Pin dependencies prior to release. Dodal 1.13.1, nexgen 0.8.4"
+        "Pin dependencies prior to release. Dodal 1.30.0, nexgen 0.9.5"
     )
 
 
 def test_pin(patched_run_pip_freeze):
     installed_versions = pin_versions.fetch_pin_versions()
     with io.StringIO() as output_file:
-        with open("test_data/setup.cfg.unpinned") as input_file:
+        with open(
+            "hyperion_other/workflows/test_data/pyproject.toml.unpinned"
+        ) as input_file:
             pin_versions.process_files(
                 input_file,
                 output_file,
-                partial(pin_versions.update_setup_cfg_line, installed_versions),
+                partial(pin_versions.update_pyproject_toml_line, installed_versions),
             )
 
-        with open("test_data/setup.cfg.pinned") as expected_file:
+        with open(
+            "hyperion_other/workflows/test_data/pyproject.toml.pinned"
+        ) as expected_file:
             assert output_file.getvalue() == expected_file.read()
